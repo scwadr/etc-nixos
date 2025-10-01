1f6eb0c3fa47a1795bd50b6c03055344a2086044
Ken Shibata <ken.shibata@kiyuri.ca>
2025-09-30 20:11:36 -0400

    minamo: disable xen, use KVM for windows passthrough

--- minamo/configuration.nix
+++ minamo/configuration.nix
@@ -47,19 +47,6 @@
 
   time.timeZone = "America/New_York";
 
-  virtualisation.xen = {
-    enable = true;
-    efi.bootBuilderVerbosity = "info"; # Adds a handy report that lets you know which Xen boot entries were created.
-    bootParams = [
-      "vga=ask" # Useful for non-headless systems with screens bigger than 640x480.
-      "dom0=pvh" # Uses the PVH virtualisation mode for the Domain 0, instead of PV.
-    ];
-    dom0Resources = {
-      memory = 4096; # memory for Domain 0, with the rest of the system memory being freely available to other domains.
-      maxVCPUs = 2; # Allows the Domain 0 to use, at most, two CPU cores.
-    };
-  };
-
   users.groups.kiyurica = { };
   users.users.kiyurica = {
     isNormalUser = true;

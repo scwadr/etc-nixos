11550ffd2aa31e7951be847ab5ee67d11f2630d4
Ken Shibata <ken.shibata@kiyuri.ca>
2025-10-02 12:50:23 -0400

    bugfix

--- home-manager/sayonara.nix
+++ home-manager/sayonara.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   # Sayonara music player configuration
   # Disables the notification for new version updates
   # Sayonara stores config in a SQLite database at ~/.config/sayonara/player.db
-  home.activation.configureSayonara = pkgs.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  home.activation.configureSayonara = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     DB_PATH="''${XDG_CONFIG_HOME:-$HOME/.config}/sayonara/player.db"
     if [ -f "$DB_PATH" ]; then
       ${pkgs.sqlite}/bin/sqlite3 "$DB_PATH" \

33c47e625840d571f4f081cb530413339cc9a549
Ken Shibata <ken.shibata@kiyuri.ca>
2025-12-07 21:58:16 -0500

    actually add joplin files to minamo impermanence

--- minamo/impermanence.nix
+++ minamo/impermanence.nix
@@ -35,6 +35,8 @@
         ".config/sayonara"
         ".config/github-copilot"
         ".config/.copilot"
+        ".config/joplin"
+        ".config/joplin-desktop"
         ".codex"
       ];
     };

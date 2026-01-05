d3b32a3ede414efc99bf98f962ca8c2ea40aca4d
Ken Shibata <ken.shibata@kiyuri.ca>
2026-01-06 00:11:39 +0900

    syncthing: cleanup and bugfix

--- syncthing.nix
+++ syncthing.nix
@@ -19,7 +19,6 @@
       "minato".id = "6ROWFH5-WMAJ5JO-TDJA22O-AOQYET7-SCRIF6T-Q6A3HMA-VP7263N-JMIIRQO";
       "suzaku".id = "5DES2YX-7XTFTK7-SGP4VRD-KVS5DAO-VPMXEC7-RDAGYKE-QDRZDDD-NS5ANAZ";
       "inaho".id = "THGLO7L-TJ4Q4UF-BE2ZERW-AXHKKSY-CAZTUJY-W5T24JT-VC7WCTR-GJPPMAH";
-      "DELL-Maker-Faire".id = "CLRJ7EG-DOL5KYF-B4U5NTI-74J647Y-QITQVKQ-OYPZP5N-KBCTLMR-6R7L3AI";
       "minamo".id = "6P75Z4H-VY2VBWM-NE3FYVF-UY7PMCV-6KOOQHD-MHB3AWR-BDAVEJI-6GBLIQA";
     };
     settings.folders = {
@@ -27,9 +26,7 @@
         id = "pugdv-kmejz";
         path = "/home/kiyurica/inaba";
         devices = [
-          "makura"
           "minato"
-          "yagoto"
           "suzaku"
           "inaho"
           "minamo"
@@ -45,35 +42,13 @@
         label = "GF-01";
         path = "/home/kiyurica/inaba/geofront";
         devices = [
-          "makura"
-          "yagoto"
           "suzaku"
           "inaho"
-          "Pixel 6a"
           "minamo"
         ];
         versioning.type = "trashcan";
         versioning.params.cleanoutDays = "0"; # never
         ignoreDelete = true;
-      };
-      "hisame" = {
-        id = "fzewo-z2hef";
-        label = "hisame";
-        path = "/home/kiyurica/inaba/hisame";
-        devices = [
-          "yagoto"
-          "suzaku"
-        ];
-      };
-      "3d-spool" = {
-        id = "qcj7e-rviwt";
-        label = "3D spool";
-        path = "/home/kiyurica/3d-spool";
-        devices = [
-          "suzaku"
-          "inaho"
-          "strawberry"
-        ];
       };
     };
   };
